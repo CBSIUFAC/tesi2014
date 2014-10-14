883fc2874ba814a9f51c33032702889d39b76112
Júnior <Jnior@172.16.108.45>
2014-10-14 08:54:19 -0500

    Getters and setters

--- tesi2014/src/test/java/tesi2014/Carro.java
+++ tesi2014/src/test/java/tesi2014/Carro.java
@@ -2,4 +2,12 @@ package tesi2014;
 
 public class Carro {
 	private int idCarro;
+
+	public int getIdCarro() {
+		return idCarro;
+	}
+
+	public void setIdCarro(int idCarro) {
+		this.idCarro = idCarro;
+	}
 }
